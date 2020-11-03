1df12e8d5e80b9a4b2f3a584cd242d01792f52cc
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-11-03 14:10:22 -0800

    css

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ export default class App extends Component {
 
   render() {
     return (
-        <div>
+        <div className="main-body">
           {
             this.state.classes.length === 0
             ? 'loading'
-            :this.state.classes.map(ffclass => <div>
+            :this.state.classes.map(ffclass => <div className="class-render" height="200" width="200">
               <div>{ffclass.name}</div>
               <div>{ffclass.cool_factor}</div>
               <div>{ffclass.role}</div>
